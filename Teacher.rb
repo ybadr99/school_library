9588d28cbee22ed4eea98c315170b8e8f371527f
ybadr99 <yousefbadr909@gmail.com>
2023-07-26 06:03:00 +0300

    Add Person | Student | Teacher classes

---
+++ Teacher.rb
@@ -0,0 +1,15 @@
+require_relative 'Person'
+
+class Teacher < Person
+  attr_accessor :specialization
+
+  def initialize(age, name = 'Unknown', parent_permissions = true, specialization)
+    super(age, name, parent_permissions)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
+
