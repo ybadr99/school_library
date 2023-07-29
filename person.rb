62989ddd675d5303b53ac2b340d7430be97bd2f8
ybadr99 <yousefbadr909@gmail.com>
2023-07-29 06:04:51 +0300

    Practice uml digram and Add relatoins between classes

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
 require './nameable'
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rental
   attr_accessor :name, :age
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,6 +9,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
   end
 
   def correct_name
