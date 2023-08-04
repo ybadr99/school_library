d92ed5febb8ab7e729ad3bfad97de61cb7e9d59b
Sumeya <sumeyaibrahim72@gmail.com>
2023-08-04 10:19:17 +0300

    Fix linters

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -1,12 +1,12 @@
 require_relative '../teacher'
 
 RSpec.describe Teacher do
-    let(:name) { 'renim' }
-    let(:age) { 23 }
-    let(:specialization) {'English'}
-    let(:teacher) { Teacher.new(specialization, age, name) }
+  let(:name) { 'renim' }
+  let(:age) { 23 }
+  let(:specialization) { 'English' }
+  let(:teacher) { Teacher.new(specialization, age, name) }
 
-    describe '#initialize' do
+  describe '#initialize' do
     it 'should initialize new teacher' do
       expect(teacher.name).to eq(name)
       expect(teacher.age).to eq(age)
@@ -15,23 +15,22 @@ RSpec.describe Teacher do
   end
 
   describe '#can use service' do
-  it 'returns true' do
-    expect(teacher.can_use_services?).to be_truthy
+    it 'returns true' do
+      expect(teacher.can_use_services?).to be_truthy
+    end
   end
-end
 
 
-describe '#to_hash' do
-it 'returns a hash with the correct keys and values' do
-  expected_hash = {
-    'id' => teacher.id,
-    'name' => name,
-    'age' => age,
-    'specialization' => specialization,
-    'type' => 'Teacher'
-  }
-  expect(teacher.to_hash).to eq(expected_hash)
-end
-end
-  
+  describe '#to_hash' do
+    it 'returns a hash with the correct keys and values' do
+      expected_hash = {
+        'id' => teacher.id,
+        'name' => name,
+        'age' => age,
+        'specialization' => specialization,
+        'type' => 'Teacher'
+      }
+      expect(teacher.to_hash).to eq(expected_hash)
+    end
+  end
 end
